0c59e749d1596d5446314d0b7ded32fa34f832ba
GeoffrayCM <muyombe@hotmail.fr>
2026-04-03 14:32:55 +0200

    Removing part4 from Hedy's repo to put it on my fork

--- part4/hbnb/app/api/v1/auth_utils.py
+++
@@ -1,7 +0,0 @@
-from flask_jwt_extended import get_jwt
-
-
-def is_admin():
-    """Return True if the current JWT belongs to an admin user."""
-    claims = get_jwt()
-    return claims.get("is_admin", False)
